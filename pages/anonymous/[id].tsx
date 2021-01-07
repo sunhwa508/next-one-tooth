415d3632155d3b73c626855e18812b3e610655b7
sunhwa lee <2017sunhwa@gmail.com>
2021-01-07 09:15:15 +0900

    주석 제거

--- pages/anonymous/[id].tsx
+++ pages/anonymous/[id].tsx
@@ -14,7 +14,7 @@ type Props = {
 const StaticPropsDetail = ({ item, errors }: Props) => {
 	if (errors) {
 		return (
-			<Layout title="Error | Next.js + TypeScript Example">
+			<Layout title="Error">
 				<p>
 					<span style={{ color: 'red' }}>Error:</span> {errors}
 				</p>
@@ -28,25 +28,17 @@ const StaticPropsDetail = ({ item, errors }: Props) => {
 export default StaticPropsDetail;
 
 export const getStaticPaths: GetStaticPaths = async () => {
-	// Get the paths we want to pre-render based on anonymous
 	const paths = samplePostData.map((user) => ({
 		params: { id: user.id.toString() },
 	}));
 
-	// We'll pre-render only these paths at build time.
-	// { fallback: false } means other routes should 404.
 	return { paths, fallback: false };
 };
 
-// This function gets called at build time on server-side.
-// It won't be called on client-side, so you can even do
-// direct database queries.
 export const getStaticProps: GetStaticProps = async ({ params }) => {
 	try {
 		const id = params?.id;
 		const item = samplePostData.find((data) => data.id === Number(id));
-		// By returning { props: item }, the StaticPropsDetail component
-		// will receive `item` as a prop at build time
 		return { props: { item } };
 	} catch (err) {
 		return { props: { errors: err.message } };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,3 @@
-// import React from 'react';
-// import Layout from '../components/layout';
-//
-// const IndexPage = () => (
-// 	<Layout title="ONE-TOOTH">
-// 		<h1>릴리즈노트</h1>
-// 	</Layout>
-// );
-//
-// export default IndexPage;
-
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { END } from 'redux-saga';
