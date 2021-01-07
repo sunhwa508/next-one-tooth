0c5e72a00bb590e18ba77745752f8884ac0832a0
sunhwa lee <2017sunhwa@gmail.com>
2021-01-07 18:27:58 +0900

    no message

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  purge:['./src/pages/**/*.ts,tsx,js', './src/components/**/*.js,ts,tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  }
+}
